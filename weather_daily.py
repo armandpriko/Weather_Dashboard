bf62acad6811412dbb4fe3715383e17a2025bf09
armandpriko <koumiprince@gmail.com>
2025-03-25 13:39:08 +0100

    Ajout d'une route d'accueil redirigeant vers l'analyse journalière et mise à jour du lien dans le template

--- weather_daily.py
+++ weather_daily.py
@@ -118,6 +118,9 @@ def daily():
 
 # ------------------ REMAINING MONTHLY ROUTES ------------------
 
+@app.route("/")
+def home():
+    return redirect(url_for("daily"))
 
 if __name__ == "__main__":
     app.run(debug=True)
